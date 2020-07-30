87b258de9552388ab88651eb45784c1028cd6d04
samytichadou <samytichadou@gmail.com>
2020-07-31 01:41:45 +0200

    add proper prop display

--- properties.py
+++ properties.py
@@ -7,7 +7,7 @@ class CatHideViewportPanelsChilds(bpy.types.PropertyGroup) :
     context : bpy.props.StringProperty(name="Context")
     original_category : bpy.props.StringProperty(name="Original Category")
     hide : bpy.props.BoolProperty(name = "Hide")
-    display : bpy.props.BoolProperty(name = "Display Panel")
+    # display : bpy.props.BoolProperty(name = "Display Panel")
 
 class CatHideViewportPanelsPanels(bpy.types.PropertyGroup) :
     '''name : StringProperty() '''
@@ -16,11 +16,13 @@ class CatHideViewportPanelsPanels(bpy.types.PropertyGroup) :
     original_category : bpy.props.StringProperty(name="Original Category")
     child_panels : bpy.props.CollectionProperty(type = CatHideViewportPanelsChilds, name="Childs")
     hide : bpy.props.BoolProperty(name = "Hide")
-    display : bpy.props.BoolProperty(name = "Display Panel")
-    child_display : bpy.props.BoolProperty(name = "Display Childs")
+    # display : bpy.props.BoolProperty(name = "Display Panel")
+    # child_display : bpy.props.BoolProperty(name = "Display Childs")
 
 class CatHideViewportPanelsCategories(bpy.types.PropertyGroup) :
     '''name : StringProperty() '''
     panels : bpy.props.CollectionProperty(type = CatHideViewportPanelsPanels, name="Panels")
     hide : bpy.props.BoolProperty(name = "Hide")
     display : bpy.props.BoolProperty(name = "Display Category")
+    panels_display : bpy.props.BoolProperty(name = "Display Panels Informations")
+    panels_index : bpy.props.IntProperty(min=-1)
