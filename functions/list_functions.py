8f53b43ccc9be152dbe604bf21da8001bf0a65df
samytichadou <samytichadou@gmail.com>
2020-08-01 01:19:40 +0200

    refresh list operator

--- functions/list_functions.py
+++
@@ -1,9 +0,0 @@
-import bpy
-
-
-def empty_lists():
-    pass
-
-
-def get_panels_original_categories():
-    pass
--- operator/refresh_lists_operator.py
+++ operator/refresh_lists_operator.py
@@ -1,7 +1,46 @@
 import bpy
 
 from ..functions.panel_functions import createPanelCategoriesProperties
-from ..functions.list_functions import empty_lists, get_panels_original_categories
+
+
+def clear_collection(collection) :
+    if len(collection)>=1:
+        for i in range(len(collection)-1,-1,-1):
+            collection.remove(i)
+
+
+def empty_lists():
+
+    winman = bpy.data.window_managers[0]
+
+    clear_collection(winman.cathide_viewport_panels_categories)
+
+
+def get_panels_original_categories():
+
+    panel_list = []
+
+    for cat in bpy.data.window_managers[0].cathide_viewport_panels_categories:
+
+        for pan in cat.panels:
+            
+            panel_list.append([pan.idname, pan.original_category])
+
+    return panel_list
+
+
+def reset_original_categories(original_cat_list):
+
+    for cat in bpy.data.window_managers[0].cathide_viewport_panels_categories:
+
+        for pan in cat.panels:
+            
+            for old_panel_entry in original_cat_list:
+
+                if pan.idname == old_panel_entry[0]:
+                    
+                    pan.original_category = old_panel_entry[1]
+                    break
 
 
 class CATHIDE_OT_refresh_lists(bpy.types.Operator):
@@ -10,8 +49,6 @@ class CATHIDE_OT_refresh_lists(bpy.types.Operator):
     bl_label = "Refresh"
     bl_options = {'REGISTER'}
 
-
-    refresh_original_categories : bpy.props.BoolProperty(default=False)
 
     @classmethod
     def poll(cls, context):
@@ -22,7 +59,7 @@ class CATHIDE_OT_refresh_lists(bpy.types.Operator):
 
         print("CatHide retrieving original categories") #debug
 
-        get_panels_original_categories()
+        original_cat_list = get_panels_original_categories()
 
         print("CatHide emptying lists") #debug
 
@@ -31,5 +68,9 @@ class CATHIDE_OT_refresh_lists(bpy.types.Operator):
         print("CatHide loading panels : ") #debug
 
         createPanelCategoriesProperties()
+
+        print("Cathide resetting original categories")
+
+        reset_original_categories(original_cat_list)
 
         return {'FINISHED'}
